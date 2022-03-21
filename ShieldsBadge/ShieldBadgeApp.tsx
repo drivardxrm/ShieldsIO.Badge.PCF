c829ad11f194351ce121d5b2551110f96b00f892
David Rivard <david.rivard@hiloenergie.com>
2022-03-20 21:21:56 -0400

    ready for first release

--- ShieldsBadge/ShieldBadgeApp.tsx
+++ ShieldsBadge/ShieldBadgeApp.tsx
@@ -1,66 +1,53 @@
 
 // eslint-disable-next-line no-use-before-define
 import * as React from 'react'
-import { Image } from '@fluentui/react'
-
 import { IInputs } from './generated/ManifestTypes'
 
 // eslint-disable-next-line no-undef
 const ShieldBadgeApp = (context:ComponentFramework.Context<IInputs>): JSX.Element => {
   const style = context.parameters.style.raw ?? 'plastic'
-  // const label = context.parameters.customlabel.raw // todo switch depending on type
-  // const message = context.parameters.message.raw // todo switch depending on type
-  const labelcolor = context.parameters.labelcolor.raw
-  const color = context.parameters.color.raw
-  const logo = context.parameters.logo.raw
-  const logocolor = context.parameters.logocolor.raw
+
+  const labelcolor = context.parameters.labelcolor.raw && context.parameters.labelcolor.raw.length > 0 ? context.parameters.labelcolor.raw : 'null'
+  const color = context.parameters.color.raw && context.parameters.color.raw.length > 0 ? context.parameters.color.raw : 'success'
+  const logo = context.parameters.logo.raw && context.parameters.logo.raw.length > 0 ? context.parameters.logo.raw : 'null'
+  const logocolor = context.parameters.logocolor.raw && context.parameters.logocolor.raw.length > 0 ? context.parameters.logocolor.raw : 'null'
+  const redirecturl = context.parameters.url.raw
 
   const getlabel = ():string => {
     let label = ''
     if (context.parameters.showLabel.raw === 'true') {
       if (context.parameters.customlabel.raw !== null) {
-        // todo switch depending on type
-        switch (context.parameters.customlabel.type) {
-          case 'Lookup.Simple':
-          case 'OptionSet':
-          case 'TwoOptions':
-            label = context.parameters.customlabel.formatted ?? ''
-            break
-          default:
-            label = context.parameters.customlabel.raw
-            break
-        }
+        label = context.parameters.customlabel.formatted ?? context.parameters.customlabel.raw
       } else {
         // return field display name
         label = context.parameters.message.attributes?.DisplayName ?? ''
       }
     }
-    return label
+    return (label ?? '').replaceAll('-', '--').replaceAll('_', '__').replaceAll(' ', '_')
   }
 
   const getMessage = ():string => {
     let message = ''
 
-    // todo switch depending on type
-    switch (context.parameters.message.type) {
-      case 'Lookup.Simple':
-      case 'TwoOptions':
-      case 'OptionSet':
-        // const options:ComponentFramework.PropertyHelper.OptionMetadata[] = context.parameters.message.attributes.Options;
-        // message = context.parameters.message.attributes?.Options.find(o => o.Value === context.parameters.message.raw)?.Label ?? ''
-        message = context.parameters.message.formatted ?? ''
-        break
-      default:
-        message = context.parameters.message.raw
-        break
-    }
+    message = context.parameters.message.formatted ?? context.parameters.message.raw
 
-    return message
+    return (message ?? '').replaceAll('-', '--').replaceAll('_', '__').replaceAll(' ', '_')
   }
 
-  const url = `https://img.shields.io/badge/${getlabel()}-${getMessage()}-${color}?style=${style}&labelColor=${labelcolor}&logo=${logo}&logoColor=${logocolor}`
+  const badgeurl = `https://img.shields.io/badge/${getlabel()}-${getMessage()}-${color}?style=${style}&labelColor=${labelcolor}&logo=${logo}&logoColor=${logocolor}`
   return (
-            <Image src={url} />
+
+    <>
+    { redirecturl && redirecturl !== '' &&
+      <a href={redirecturl} target="_blank" rel="noopener noreferrer">
+        <img src={badgeurl} />
+      </a>
+    }
+    { (!redirecturl || redirecturl === '') &&
+      <img src={badgeurl} />
+    }
+
+    </>
   )
 }
 
